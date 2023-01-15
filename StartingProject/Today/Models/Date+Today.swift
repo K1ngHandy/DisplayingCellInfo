9fdf26b6edf5cc389574038ef5e47fd48cabfe7a
Steve H <a.king.life@pm.me>
2023-01-14 20:11:15 -0500

    date and time format

---
+++ StartingProject/Today/Models/Date+Today.swift
@@ -0,0 +1,29 @@
+//
+//  Date+Today.swift
+//  Today
+//
+//  Created by Stephen Handy on 1/14/23.
+//
+
+import Foundation
+
+extension Date {
+    var dayAndTimeText: String {
+        let timeText = formatted(date: .omitted, time: .shortened)
+        if Locale.current.calendar.isDateInToday(self) {
+            let timeFormat = NSLocalizedString("Today is %@", comment: "Today is at time format string")
+            return String(format: timeFormat, timeText)
+        } else {
+            let dateText = formatted(.dateTime.month(.abbreviated).day())
+            let dateAndTimeFormat = NSLocalizedString("%@ at %0", comment: "Date and time format string")
+            return String(format: dateAndTimeFormat, dateText: timeText)
+        }
+    }
+    var dayText: String {
+        if Locale.current.calendar.isDateInToday(self) {
+            return NSLocalizedString("Today", comment: "Today due date description")
+        } else {
+            return formatted(.dateTime.month().day().weekday(.wide))
+        }
+    }
+}
